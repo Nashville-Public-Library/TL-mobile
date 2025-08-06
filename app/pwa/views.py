f8ddbf1fcf0837c25f25e4a6a929bb106338af6c
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-08-06 16:24:41 -0500

    add image to orientation change prompt

--- app/pwa/views.py
+++ app/pwa/views.py
@@ -3,7 +3,7 @@ from flask import render_template, make_response
 from app import app
 from app.pwa.pod import Podcast
 
-VERSION = "0.4.14"
+VERSION = "0.4.15"
 
 @app.route('/', methods=['GET'])
 def pwa():
