648813e01686cf16f3cb648ed4fc9b9ad99fb181
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-08-14 17:09:09 -0500

    reorder podcast categories alphabetically

--- app/pwa/views.py
+++ app/pwa/views.py
@@ -3,7 +3,7 @@ from flask import render_template, make_response
 from app import app
 from app.pwa.pod import Podcast
 
-VERSION = "0.4.28"
+VERSION = "0.4.29"
 
 @app.route('/', methods=['GET'])
 def pwa():
