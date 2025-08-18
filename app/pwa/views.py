9f3b8feca96f0d0b8f9bab3c46b4718396107c63
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-08-18 17:35:20 -0500

    try this?

--- app/pwa/views.py
+++ app/pwa/views.py
@@ -3,7 +3,7 @@ from flask import render_template, make_response
 from app import app
 from app.pwa.pod import Podcast
 
-VERSION = "0.4.33"
+VERSION = "0.4.34"
 
 @app.route('/', methods=['GET'])
 def pwa():
