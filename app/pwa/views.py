b3b7268d485884c21cc16d392d2014b8670eae00
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-08-28 19:13:10 -0500

    attempt CSS fix on daily schedule pages where text scroll was visible above header

--- app/pwa/views.py
+++ app/pwa/views.py
@@ -3,7 +3,7 @@ from flask import render_template, make_response
 from app import app
 from app.pwa.pod import Podcast
 
-VERSION = "0.4.43"
+VERSION = "0.4.44"
 
 @app.route('/', methods=['GET'])
 def pwa():
