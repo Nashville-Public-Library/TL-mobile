bbc78f621e022a2d138b55cf9e572e20dde7678b
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-09-02 15:10:34 -0500

    style updates for Privacy link, bump version

--- app/pwa/views.py
+++ app/pwa/views.py
@@ -3,7 +3,7 @@ from flask import render_template, make_response
 from app import app
 from app.pwa.pod import Podcast
 
-VERSION = "0.4.49"
+VERSION = "0.5.0"
 
 @app.route('/', methods=['GET'])
 def pwa():
