4117ecd491bc6f82e3337de8f4bc289b54fd27c9
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-09-21 10:25:32 -0500

    bump version

--- app/pwa/views.py
+++ app/pwa/views.py
@@ -3,7 +3,7 @@ from flask import render_template, make_response
 from app import app
 from app.pwa.pod import Podcast
 
-VERSION = "0.5.3"
+VERSION = "0.5.4"
 
 @app.route('/', methods=['GET'])
 def pwa():
