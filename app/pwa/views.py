11030329c0b6b8c3ab7b48d09b94ac3bdb1005a8
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-10-21 16:30:05 -0500

    minor changes to "about" screen

--- app/pwa/views.py
+++ app/pwa/views.py
@@ -3,7 +3,7 @@ from flask import render_template, make_response
 from app import app
 from app.pwa.pod import Podcast
 
-VERSION = "0.5.7"
+VERSION = "0.5.8"
 
 @app.route('/', methods=['GET'])
 def pwa():
