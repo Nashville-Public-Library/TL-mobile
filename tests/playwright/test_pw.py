e9957d9b9d9f9a8010baa459d77d259f951c64ef
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-11-24 23:44:47 -0600

    add tests

--- tests/playwright/test_pw.py
+++ tests/playwright/test_pw.py
@@ -11,22 +11,37 @@ def test_first_load_2(desktop: Page, server):
 
 def test_mobile_installed_1(mobile_installed: Page, server):
     '''checking if home screen logo is on page'''
-    mobile_installed.goto(server)
     mobile_installed.wait_for_selector(".logo-container")
     assert mobile_installed.locator(".logo-container").count() > 0
 
 def test_schedule_1(mobile_installed: Page, server):
-    mobile_installed.goto(server)
+    '''navigate to /schedule page and check if various content is loaded'''
     mobile_installed.evaluate("window.location.hash = '#/schedule'")
     mobile_installed.wait_for_selector(".dailyScheduleLinksContainer")
     assert mobile_installed.get_by_text("Schedules").is_visible()
     assert mobile_installed.get_by_text("Tuesday").is_visible()
     assert mobile_installed.get_by_text("Broadcast Schedule").is_visible()
 
-def test_podcast_1(mobile_installed: Page, server):
-    mobile_installed.goto(server)
+def test_podcasts_1(mobile_installed: Page, server):
+    '''navigate to /podcasts page and check if various content is loaded'''
     mobile_installed.evaluate("window.location.hash = '#/podcasts'")
-    mobile_installed.wait_for_selector(".podcastIndividual")
-    mobile_installed.wait_for_selector("#categorySelector")
+    mobile_installed.wait_for_selector(".podcastIndividual", state="visible")
+    mobile_installed.wait_for_selector("#categorySelector", state="visible")
     assert mobile_installed.locator(".podcastIndividual").count() > 0
     assert mobile_installed.locator("#categorySelector").count() > 0
+
+def test_podcast_individual_1(mobile_installed: Page, server):
+    '''navigate to /podcasts page, load individual podcast, check if various content is loaded. Checklist chosen at random'''
+    mobile_installed.evaluate("window.location.hash = '#/podcasts'")
+    mobile_installed.wait_for_selector("#checklist", state="visible")
+    mobile_installed.locator("#checklist").click()
+    mobile_installed.wait_for_selector("#podcastIndividualTitle", state="visible")
+    assert "Checklist" in mobile_installed.locator("#podcastIndividualTitle").inner_text()
+
+def test_podcast_individual_2(mobile_installed: Page, server):
+    '''navigate to /podcasts page, load individual podcast, check if various content is loaded. Checklist chosen at random.'''
+    mobile_installed.evaluate("window.location.hash = '#/podcasts'")
+    mobile_installed.wait_for_selector("#checklist", state="visible")
+    mobile_installed.locator("#checklist").click()
+    mobile_installed.wait_for_selector(".podcastEpisodeTitle", state="visible")
+    assert mobile_installed.locator(".podcastEpisodeTitle").count() > 0
