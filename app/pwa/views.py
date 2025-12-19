82f39b1690addc170a61bacb44fd1380b9b36c17
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-12-19 14:18:08 -0600

    remove unnecessary route, imports

--- app/pwa/views.py
+++ app/pwa/views.py
@@ -1,7 +1,4 @@
-import time
-
 from flask import render_template, make_response, request
-import requests
 
 from app import app
 from app.pwa.pod import Podcast
@@ -30,21 +27,6 @@ def serve_app_js():
     response.headers["Content-Type"] = "application/javascript"
     response.headers["Cache-Control"] = "no-cache"
     return response
-
-@app.route('/podcasts', methods=['POST'])
-def podcasts():
-    shows = {"AARP Report": "aarp", "Able Living": "able", "Around the World": "aroundworld", "Atlantic": "atlantic", "Book Page": "bookpage", 
-             "Checklist": "checklist", "Hourly Weather Forecast": "cirrus", "Community News": "community", "Consumer Reports": "consumer", 
-             "Diabetic News": "diabetes", "Discover": "discover", "Economist": "economist", "Entertainment Weekly": "entertainment", 
-             "Eyes on Success": "eyes", "Fortune": "fortune", "Historical View": "historical", "An Hour of Short Stories": "hourshortstories", 
-             "Independent Living": "independent", "Nashville Ledger": "ledger", "LGBTQ News & Culture": "lgbt", "Men's Hours": "mens", 
-             "Money Talk": "moneytalk", "National Geographic": "nationalgeo", "Newsweek": "newsweek", "New Yorker": "newyorker", 
-             "New York Times": "nyt", "Tennessean Opinions": "opinion", "People": "people", "Pet Potpourri": "pet", "PNS 2025 Talks": "pnstalks", 
-             "PNS Daily Newscast": "pns", "PNS Yonder Report": "pnsyonder", "Poetry in the Air": "poetry", "Prevention": "prevention", 
-             "Reader's Digest": "readersdigest", "Rolling Stone": "rollingstone", "Nashville Scene": "scene", "New Scientist": "science", 
-             "Smithsonian": "smithsonian", "Spotlight on Sports": "sports", "Tennessean": "tennessean", "Time": "time", "Town & Country": "town", 
-             "Vanity Fair": "vanity", "Wired": "wired", "Woman's World": "woman", "Wall Street Journal": "wsj"}
-    return {"shows": shows}
 
 @app.route('/podcasts/info/<podcast>', methods=['POST'])
 def podcasts_info(podcast):
